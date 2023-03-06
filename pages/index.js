56bfb9c24b2c447e8d381a1c4232808041c027ea
Unfamous Dyson <dougdyson@gmail.com>
2023-03-05 21:36:58 -0800

    update stuff

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm an aspiring fullstack developer and passionate explorer of ideas.</p>
+        <p>I'm an aspiring fullstack developer and passionate explorer of new ideas.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
